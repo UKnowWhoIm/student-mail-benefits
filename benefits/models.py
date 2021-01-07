69a440448c77fe558957a20fa0b8326a41fd7f9f
Sidharth <sidforjee@gmail.com>
2021-01-07 20:18:00 +0530

    added barebone stuff

--- benefits/models.py
+++ benefits/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+
 class Benefit(models.Model):
-    pass
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, related_name="benefits", on_delete=models.SET_NULL, null=True)
+    link = models.CharField(max_length=100)
