828672b7f06ea8b86970ec00aa3257d535d67594
abhinavk001 <godofwars0017@gmail.com>
2021-01-09 01:23:14 +0530

    Django Rest API implement

--- benefits/urls.py
+++ benefits/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import BenefitView
+
 
 urlpatterns = [
-
+    path('benefits', BenefitView.as_view()),
 ]
--- benefits/views.py
+++ benefits/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BenefitSerializer
+from .models import Benefit
 
-# Create your views here.
+
+class BenefitView(generics.ListAPIView):
+    queryset = Benefit.objects.all()
+    serializer_class = BenefitSerializer
