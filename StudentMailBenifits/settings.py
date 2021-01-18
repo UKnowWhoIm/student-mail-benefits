7e916e8c52ca035fa2390e8cad802bdc86ff8c4a
Sidharth <sidforjee@gmail.com>
2021-01-19 03:04:51 +0530

    added setup for static files

--- StudentMailBenifits/settings.py
+++ StudentMailBenifits/settings.py
@@ -49,10 +49,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'StudentMailBenifits.urls'
@@ -144,3 +146,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = "/staticfiles/"
