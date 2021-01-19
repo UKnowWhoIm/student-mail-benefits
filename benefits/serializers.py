6ae91024f522f971b0d833e662301f2c0b08983a
Sidharth <sidforjee@gmail.com>
2021-01-19 20:55:32 +0530

    added highlight

--- benefits/serializers.py
+++ benefits/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from .models import Benefit, Category
+import json
 
 
 class BenefitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Benefit
         exclude = ["is_verified"]
+
+    def validate_highlights(self, value):
+        try:
+            obj = json.loads(value)
+            if type(obj) != list:
+                raise serializers.ValidationError("Highlights must Be JSON list")
+        except json.JSONDecodeError:
+            raise serializers.ValidationError("Invalid JSON")
+        return value
 
 
 class CategorySerializer(serializers.ModelSerializer):
