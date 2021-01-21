9f2cca09c2bfa6b9bd0ec8b03ddf4968e0354321
Sidharth <sidforjee@gmail.com>
2021-01-22 02:29:27 +0530

    added contribution model

--- benefits/models.py
+++ benefits/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+def check_promotion(email, count):
+    # TODO check for
+    pass
+
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
@@ -19,3 +24,26 @@ class Benefit(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Contribution(models.Model):
+    email = models.EmailField(null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    contribution = models.JSONField()
+    benefit = models.ForeignKey(Benefit, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=False)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.approved:
+            benefit = Benefit.objects.get(id=self.benefit.id)
+            if self.contribution:
+                # Update
+                benefit.update(**self.contribution)
+            else:
+                # Create
+                benefit.update(is_verified=True)
+
+            if self.email:
+                check_promotion(len(Contribution.objects.filter(email=self.email, approved=True)), self.email)
+
+        super().save(force_insert, force_update, using, update_fields)
