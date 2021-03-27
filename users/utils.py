b9ab436e961293d04d03d6bc6332fb01f8ecb213
Sidharth <sidforjee@gmail.com>
2021-03-27 18:36:20 +0530

    added maintainer group

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,5 @@
 import os
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.template.loader import render_to_string
 from django.utils.datetime_safe import datetime
 from commons.email import send_email_to_recipient
@@ -7,9 +7,10 @@ from .models import MailList
 
 
 def setup_new_user(instance, email):
+    maintainer_group = Group.objects.get(name='maintainer')
     instance.email = instance.username = email
     instance.is_staff = True
-    # TODO Setup permissions
+    instance.groups.add(maintainer_group)
 
 
 def get_min_contributions():
@@ -49,5 +50,3 @@ def check_promotion(count, email):
     if mail_data and (datetime.now() - mail_data[0].last_sent_time).total_seconds() >= diff or not mail_data:
         if not User.objects.filter(username=email, is_staff=True) and count >= get_min_contributions():
             invite_as_staff(email, mail_data)
-
-
