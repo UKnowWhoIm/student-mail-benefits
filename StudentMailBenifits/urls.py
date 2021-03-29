fa8547899554fe8cde173f3699c2a48128220e2a
Sidharth <sidforjee@gmail.com>
2021-03-29 22:31:06 +0530

    moved api to /api, created files for new templates

--- StudentMailBenifits/urls.py
+++ StudentMailBenifits/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .home_view import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("benefits.urls")),
-    path("users/", include("users.urls"))
+    path("api/", include("benefits.urls")),
+    path("users/", include("users.urls")),
+    path("", home_view)
 ]
